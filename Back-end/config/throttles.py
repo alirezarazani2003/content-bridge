f14d160b5e0d93b057bf233e48e41b6849f793d8
nastaran <nastaran81dr@gmail.com>
2025-08-04 13:17:40 -0700

    add rate limite for all endpoints and otp

---
+++ Back-end/config/throttles.py
@@ -0,0 +1,33 @@
+from rest_framework.throttling import BaseThrottle
+from django.core.cache import cache
+from datetime import timedelta
+from django.utils import timezone
+
+
+class RequestOTPThrottle(BaseThrottle):
+    scope = 'otp_request'
+
+    def __init__(self):
+        self.rate = '5/minute'
+
+    def allow_request(self, request, view):
+        email = request.data.get('email')
+
+        if email is None:
+            return True
+
+        key = f"otp_request:{email}"
+        current_time = timezone.now()
+
+        request_times = cache.get(key, [])
+
+        request_times = [timestamp for timestamp in request_times if timestamp >
+                         current_time - timedelta(minutes=5)]
+
+        if len(request_times) >= 5:
+            return False
+
+        request_times.append(current_time)
+        cache.set(key, request_times, timeout=60*5)
+
+        return True
