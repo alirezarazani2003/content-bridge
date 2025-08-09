336ac759f8a495627d6dbda4c3a28a69ea82fdb6
nastaran <nastaran81dr@gmail.com>
2025-08-09 02:19:30 -0700

    add validators for serializers

--- Back-end/auth_app/serializers.py
+++ Back-end/auth_app/serializers.py
@@ -1,19 +1,22 @@
+from core.validator import email_validator, otp_validator
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+
 class RequestOTPSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    email = serializers.EmailField(validators=[email_validator])
     purpose = serializers.ChoiceField(choices=["verify", "login", "reset"])
+
 
 class VerifyOTPSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    otp = serializers.CharField()
+    email = serializers.EmailField(validators=[email_validator])
+    otp = serializers.CharField(validators=[otp_validator])
     purpose = serializers.ChoiceField(choices=["verify", "login", "reset"])
-
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email', 'phone', 'is_verified')
+        fields = ('id', 'first_name', 'last_name',
+                  'email', 'phone', 'is_verified')
